aaa3fb023cbbfa84435326261649a43e6fdc05a2
Diljeet Singh <jeetds1994@gmail.com>
2017-07-21 09:26:50 -0400

    done

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
   // call functions here
+submitForm();
 });
 
 // define functions here
+function submitForm(){
+
+  $('form').on('submit', function(event){
+    let something = $('#item').val();
+    $('ol').append('<li>' + something + '</li>');
+  event.preventDefault();
+  });
+}
